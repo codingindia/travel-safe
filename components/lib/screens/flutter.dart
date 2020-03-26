04903b5f25afcf72adf71be0771917b0f7e7ca88
Samriddhi25 <samriddhiagarwal12@gmail.com>
2020-03-26 20:37:05 +0530

    feat: Backend connectivity

---
+++ components/lib/screens/flutter.dart
@@ -0,0 +1,32 @@
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+Future<FirebaseUser> _handleSignIn() async {
+  final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+
+  final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+  print("signed in " + user.displayName);
+  return user;
+
+}
+/*
+_handleSignIn()
+    .then((FirebaseUser user) => print(user))
+    .catchError((e) => print(e));
+
+  final FirebaseUser users = (await _auth.createUserWithEmailAndPassword(
+      email: 'an email',
+      password: 'a password',
+    ))
+        .user;
+        
+*/
