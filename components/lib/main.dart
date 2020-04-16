aa950d9662139e43c5e7eef7855f5bc5025d3740
Yusuf Adefolahan <yusufadefolahan@gmail.com>
2020-04-16 18:43:56 +0100

    Created splash screen. (Solved) (#41)
    * added splash screen
    
    * removed unused codes

--- components/lib/main.dart
+++ components/lib/main.dart
@@ -1,75 +1,67 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:travelsafe/panicButton.dart';
 import 'package:travelsafe/screens/maps.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:share/share.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    // Start splash screen
+    return new SplashScreen(
+        seconds: 14,
+        navigateAfterSeconds: new MyHomePage(),
+        title: new Text('Welcome To Travel Safe',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20.0
+          ),),
+        image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("Flutter Splash Screen"),
+        loaderColor: Colors.red
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
+
   final Geolocator geolocator = Geolocator()
     ..forceAndroidLocationManager;
   Position _currentPosition;
 
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Flutter Demo Home Page"),
+          automaticallyImplyLeading: false
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: new Center(
+
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
@@ -103,7 +95,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
+
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(
@@ -133,9 +127,11 @@ class _MyHomePageState extends State<MyHomePage> {
   _shareMap() {
     final RenderBox box = context.findRenderObject();
     Share.share(
-        'https://www.google.com/maps/search/?api=1&query=${_currentPosition.latitude},${_currentPosition.longitude}',
+        'https://www.google.com/maps/search/?api=1&query=${_currentPosition
+            .latitude},${_currentPosition.longitude}',
         sharePositionOrigin:
         box.localToGlobal(Offset.zero) &
         box.size);
   }
+
 }
