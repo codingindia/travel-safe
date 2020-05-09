0a8d0dc6e3f6905b2d09ce1ccea9838672cca759
Akshay <akshay2108akshay@gmail.com>
2020-05-09 08:09:48 +0530

    Main Screen Part

---
+++ master/lib/screens/Group.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class group extends StatefulWidget {
+  @override
+  _groupState createState() => _groupState();
+}
+
+class _groupState extends State<group> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ master/lib/screens/Track.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Track extends StatefulWidget {
+  @override
+  _TrackState createState() => _TrackState();
+}
+
+class _TrackState extends State<Track> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
