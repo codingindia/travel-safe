1693c17191ea52ed9780e53a1401288485d2093c
Rukmini-Meda <rukmini.m18@iiits.in>
2020-05-22 19:52:25 +0530

    About Us screen done

--- master/lib/screens/aboutUs.dart
+++ master/lib/screens/aboutUs.dart
@@ -1,4 +1,7 @@
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutUs extends StatefulWidget {
   @override
@@ -8,9 +11,30 @@ class AboutUs extends StatefulWidget {
 class _AboutUsState extends State<AboutUs> {
 
   bool need = false, mission = false, working = false, contributors = false;
+  dynamic data;
+
+  Future<dynamic> getData() async{
+    final DocumentReference document = Firestore.instance.collection("contributors").document("LE7EwutFHF15yEcQnLyK");
+
+    await document.get().then<dynamic>(
+      (DocumentSnapshot snap) async{
+        setState(() {
+          data = snap.data;
+        });
+      }
+    );
+  }
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+  
   
   Widget _displayLogo(){
+    String content = "Travel-safe simplifies safety for women in the digital world.";
     return Container(
+      padding: EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
             Padding(
@@ -29,25 +53,77 @@ class _AboutUsState extends State<AboutUs> {
                 ),
               ),
             ),
-          Padding(
-            padding: EdgeInsets.symmetric(),
-            child: Text("Travel-safe simplifies safety for women in the digital world."),
+          Container(
+      padding: EdgeInsets.all(15),
+      child: RichText(
+        text: TextSpan(
+          text: content,
+          style: TextStyle(
+            fontSize: 15,
+          wordSpacing: 5,
+          height: 1.5,
+          color: Colors.black,
           ),
+        ),
+      ),
+      )
         ],
       )
     );
   }
 
   Widget getNeedWidget(){
-    return Container(child: Text("Need"),);
+    String content = "Being Concerned with the increasing brutality and insecurities faced by women during their travel, Despite of many Safety apps, none of them properly solves this problem.Travel Safe- A Flutter Application will help in preventing such instances and will promote safe and secure travel.If you think that your notable tech skills can help women of our country travel safely, Please be a part of Travel Safe and join our mission to make the world a safer place.";
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: RichText(
+        text: TextSpan(
+          text: content,
+          style: TextStyle(
+            fontSize: 13,
+          wordSpacing: 5,
+          height: 1.5,
+          color: Colors.black,
+          ),
+        ),
+      ),
+      );
   }
 
   Widget getMissionWidget(){
-    return Container(child: Text("mission"),);
+    String content = "Our mission is to help women to be safe using technology. We aim to reach out to as many women as possible and be their courage.";
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: RichText(
+        text: TextSpan(
+          text: content,
+          style: TextStyle(
+            fontSize: 13,
+          wordSpacing: 5,
+          height: 1.5,
+          color: Colors.black,
+          ),
+        ),
+      ),
+      );
   }
 
   Widget getWorkingWidget(){
-    return Container(child: Text("working"),);
+    String content = "Share live location with a friend or in a group.Use the Panic Button to send the alarm to all the people with whom locations is being shared. Stop sharing location when you think you are safe";
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: RichText(
+        text: TextSpan(
+          text: content,
+          style: TextStyle(
+            fontSize: 13,
+          wordSpacing: 5,
+          height: 1.5,
+          color: Colors.black,
+          ),
+        ),
+      ),
+      );
   }
 
   Widget getContributorsWidget(){
@@ -56,6 +132,7 @@ class _AboutUsState extends State<AboutUs> {
         children: <Widget>[
           Row(
             children: <Widget>[
+                data.isEmpty?Container():Text(data['name'])
             ],
           ),
         ],
@@ -68,13 +145,13 @@ class _AboutUsState extends State<AboutUs> {
     return GestureDetector(
       onTap: (){
         setState(() {
-          if (text == "need"){
+          if (text == "Need for TRAVEL SAFE"){
             this.need = !this.need;
           }
-          else if(text == "mission"){
+          else if(text == "Our Mission"){
             this.mission = !this.mission;
           }
-          else if(text == "working"){
+          else if(text == "How does it work?"){
             this.working = !this.working;
           }
           else{
@@ -88,15 +165,16 @@ class _AboutUsState extends State<AboutUs> {
         elevation: 3,
         child: InkWell(
           child: Container(
-          padding: EdgeInsets.all(20),
-          width: 450,
-          height: 60,
+          padding: EdgeInsets.all(16),
+          width: 650,
+          height: 80,
           child: Row(
           children: <Widget>[
             Text(
               text,
               style: TextStyle(
-                fontSize: 17,
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
               ),
             
             ),
@@ -110,28 +188,67 @@ class _AboutUsState extends State<AboutUs> {
   }
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TRAVEL SAFE"),
-        backgroundColor: Colors.pinkAccent,
+        title: Text("Travel-safe"),
+        backgroundColor: Colors.lightBlue,
       ),
-      body: Container(
-        child: Column(children: <Widget>[
-          _displayLogo(),
-          SizedBox(height: 20),
-          _card("need"),
-          this.need?
-          getNeedWidget():Container(),
-          _card("mission"),
-          this.mission?getMissionWidget():Container(),
-          _card("working"),
-          this.working?getWorkingWidget():Container(),
-          _card("contributors"),
-          this.contributors?getContributorsWidget():Container(),
-          // _ourMission(),
-          // _contributors(),
-        ],)
+      body: SingleChildScrollView(
+            child: Column(children: <Widget>[
+               _displayLogo(),
+              SizedBox(height: 20),
+              _card("Need for TRAVEL SAFE"),
+              this.need?
+                    getNeedWidget():Container(),
+              _card("Our Mission"),
+              this.mission?getMissionWidget():Container(),
+              _card("How does it work?"),
+              this.working?getWorkingWidget():Container(),
+              _card("Our Contributors"),
+              this.contributors?StreamBuilder<QuerySnapshot>(
+                  stream: Firestore.instance.collection("contributors").snapshots(),
+                  builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snap){
+                    if(snap.hasError){
+                      return Text("Error: ${snap.error}");
+                    }
+                    switch (snap.connectionState) {
+                      case ConnectionState.waiting:return Text("Loading ...");
+                      default: 
+                      return GridView.count(
+                        shrinkWrap: true,
+                        crossAxisCount: 2,
+                        childAspectRatio: 1.0,
+                        padding: EdgeInsets.all(2.0),
+                        children: snap.data.documents.map((DocumentSnapshot document){
+                            return Container(
+                              padding: EdgeInsets.all(10),
+                              child: Column(
+                                  children: <Widget>[
+                                    Padding(padding: EdgeInsets.all(5),
+                                      child: CircularProfileAvatar(
+                                      document["profilepic"],
+                                      radius: 35,
+                                      backgroundColor: Colors.transparent,
+                                      elevation: 5,
+                                      cacheImage: true,
+                                      onTap: (){
+                                        launch(document["github"]);
+                                      },
+                                    ),
+                                    ),   
+                                    Text(document["name"]),
+                                  ],
+                                )
+                            );
+                        }
+                        ).toList(),
+                      ); 
+                      break;
+                    }
+                  }
+                ):Container()
+            ]),
       ),
     );
   }
--- master/lib/screens/introPage.dart
+++ master/lib/screens/introPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travelsafe/screens/login.dart';
 import 'package:travelsafe/screens/register.dart';
 // import 'package:travelsafe/screens/login.dart';
 import 'package:carousel_slider/carousel_slider.dart';
