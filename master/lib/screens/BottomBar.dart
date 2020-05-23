b35bc98939d22788f78c5900d06a2e0805c0f057
Akshay <akshay2108akshay@gmail.com>
2020-05-23 12:41:55 +0530

    Updated Bottom.dart

--- master/lib/screens/BottomBar.dart
+++ master/lib/screens/BottomBar.dart
@@ -1,103 +1,127 @@
-
-
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import './LandingScreen.dart';
-import './track.dart';
-import './group.dart';
+import 'package:share/share.dart';
+import 'package:travelsafe/screens/LandingScreen.dart';
+import 'package:travelsafe/screens/alert.dart';
+import 'package:travelsafe/screens/group.dart';
+import 'package:travelsafe/screens/track.dart';
 
-class BottomBar extends StatelessWidget{
+class BottomBar extends StatefulWidget {
   @override
-   Widget build(BuildContext context){
-     AssetImage assetimg =  AssetImage('assets/images/home.png');
-      Image image = Image(image: assetimg);
-      return Container(
-        decoration: outerBoxDecoration(), 
-        child: Row(
-           children: <Widget>[
-               Expanded(
-                  child: GestureDetector(
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Track()),
-                      );
-                    },
-                    child: Text("Track",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                    decoration: TextDecoration.none,
-                   fontSize: 25.0,
-                   color: Colors.black,
-                   fontWeight: FontWeight.w700,
-                 ),
-                ),
-             ),
-             ),
-            
-              Expanded(
-                child: GestureDetector(
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => LandingScreen()),
-                      );
-                    },
-                child: Container(
-                child: image,
-                height: 70.0,
-                width: 70.0,
-                decoration: homeBoxDecoration(), 
-                ) ,
-             ),
-             ),
+  _BottomBarState createState() => _BottomBarState();
+}
 
-              Expanded(
-                 child: GestureDetector(
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Group()),
-                      );
-                    },
-                child:  Text("Groups",
-                 textAlign: TextAlign.center,
-                 style: TextStyle(
-                  decoration: TextDecoration.none,
-                   fontSize: 25.0,
-                   color: Colors.black,
-                   fontWeight: FontWeight.w700
-                 ),
-                 ) 
-            )
+class _BottomBarState extends State<BottomBar> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+  int pageIndex = 0;
+
+  final Track _track = Track();
+  final LandingScreen _landingScreen = LandingScreen();
+  final Group _group = Group();
+  final Share _share = Share();
+  final Alert _alert = Alert();
+
+  Widget _showPage = Track();
+
+  Widget _pageChooser(int page) {
+    switch (page) {
+      case 0:
+        return _track;
+        break;
+      case 1:
+        return _landingScreen;
+        break;
+      case 2:
+        return _group;
+        break;
+    }
+  }
+
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
+  List<GlobalKey<NavigatorState>> _navigatorKeys = [
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab =
+            !await _navigatorKeys[pageIndex].currentState.maybePop();
+
+        // let system handle back button if we're on the first route
+        return isFirstRouteInCurrentTab;
+      },
+      child: Scaffold(
+          bottomNavigationBar: CurvedNavigationBar(
+            index: pageIndex,
+            key: _bottomNavigationKey,
+            backgroundColor: Colors.white,
+            color: Colors.grey.shade800,
+            items: <Widget>[
+              Icon(
+                Icons.location_on,
+                size: 25.0,
+                color: Colors.white,
               ),
-     ],
-     )
-     );
-   }
-}
+              Icon(
+                Icons.home,
+                size: 25.0,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.notifications_active,
+                size: 25.0,
+                color: Colors.white,
+              ),
+            ],
+            onTap: (int tappedIndex) {
+              setState(() {
+                pageIndex = tappedIndex;
+                // _showPage = _pageChooser(tappedIndex);
+              });
+            },
+          ),
+          body: Stack(
+            children: <Widget>[
+              _buildOffstageNavigator(0),
+              _buildOffstageNavigator(1),
+              _buildOffstageNavigator(2),
+            ],
+          )
+          //_showPage,
+          ),
+    );
+  }
 
-BoxDecoration homeBoxDecoration() {
-  return BoxDecoration(
-    border: Border(
-      left: 
-      BorderSide( //                   <--- left side
-          color: Colors.grey[400],
-          width: 7.0,
-        ),
-      right: 
-      BorderSide( //                   <--- left side
-          color: Colors.grey[400],
-          width: 7.0,
-        ),
-    ),
-  );
-}
+  Map<String, WidgetBuilder> _routeBuilders(BuildContext context, int index) {
+    return {
+      '/': (context) {
+        return [
+          Track(),
+          LandingScreen(),
+          Group(),
+        ].elementAt(index);
+      },
+    };
+  }
 
-BoxDecoration outerBoxDecoration() {
-  return BoxDecoration(
-    border: Border.all(
-      width:7.0,
-      color: Colors.grey[400],
-    ),
-  );
+  Widget _buildOffstageNavigator(int index) {
+    var routeBuilders = _routeBuilders(context, index);
+
+    return Offstage(
+      offstage: pageIndex != index,
+      child: Navigator(
+        key: _navigatorKeys[index],
+        onGenerateRoute: (routeSettings) {
+          return MaterialPageRoute(
+            builder: (context) => routeBuilders[routeSettings.name](context),
+          );
+        },
+      ),
+    );
+  }
 }
