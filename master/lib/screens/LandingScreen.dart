c1f3fe281edc9cfd268e0e8d1eee6cc392f67ba9
Akshay <akshay2108akshay@gmail.com>
2020-05-23 12:43:11 +0530

    Updated LandingScreen.dart

--- master/lib/screens/LandingScreen.dart
+++ master/lib/screens/LandingScreen.dart
@@ -1,73 +1,55 @@
 import 'package:flutter/material.dart';
-import './BottomBar.dart';
-import './alert.dart';
-import './share.dart';
+import 'package:travelsafe/screens/alert.dart';
+import 'package:travelsafe/screens/share.dart';
 
+void main() => runApp(LandingScreen());
 
-class LandingScreen extends StatelessWidget{
-   @override
-   Widget build(BuildContext context){
-
-     AssetImage share =  AssetImage('assets/images/sharelogo.png');
-      Image shareImage = Image(image: share);
-    AssetImage alert =  AssetImage('assets/images/alertlogo.png');
-      Image alertImage = Image(image: alert);
-      return Container(
-        color: Colors.white,
-        padding:EdgeInsets.only(top:120.0),
-
-        child: 
-         Column(
-           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-           children: <Widget>[
-
-             GestureDetector(
-               onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Share()),
-                );
-               },
-               child: Expanded(
-               child: Container(
-                    child: shareImage,
-                    height: 200.0,
-                    width: 200.0,
-                 )
-               )
-             ),
-
-             GestureDetector(
-               onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Alert()),
-                );
-               },
-              
-               child: Expanded(
-                 child: Container(
-                    child: alertImage,
-                     height: 200.0,
-                    width: 200.0,
-                 )
-               )
-             ),
-             
-            
-             Expanded(
-              child: 
-                Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Container(
-                    child: BottomBar(),
+class LandingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage share = AssetImage('assets/images/sharelogo.png');
+    Image shareImage = Image(image: share);
+    AssetImage alert = AssetImage('assets/images/alertlogo.png');
+    Image alertImage = Image(image: alert);
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Container(
+                height: 150.0,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Share()));
+                  },
+                  child: Image(
+                    image: AssetImage('assets/images/sharelogo.png'),
+                  ),
                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Container(
+                height: 150.0,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Alert()));
+                  },
+                  child: Image(
+                    image: AssetImage('assets/images/alertlogo.png'),
+                  ),
                 ),
-                
-               )
-        ],)
-
-        
-      );
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-   }
+}
