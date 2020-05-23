9c94329ffa847ac6e67ba313d0e4a48bb368c7b9
Akshay <akshay2108akshay@gmail.com>
2020-05-23 12:47:13 +0530

    Updated track.dart

--- master/lib/screens/track.dart
+++ master/lib/screens/track.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './BottomBar.dart';
+
+void main() => runApp(Track());
 
 class Track extends StatefulWidget {
   @override
@@ -9,30 +10,13 @@ class Track extends StatefulWidget {
 class _TrackState extends State<Track> {
   @override
   Widget build(BuildContext context) {
-        return Container(
-      alignment: Alignment.center,
-      color: Colors.white,
-       child: Column(
-         children: <Widget>[
-           Text("This is Tracks Screens",
-           style: TextStyle(
-          decoration: TextDecoration.none
-         )
-        ),
-
-
-          Expanded(
-              child: Align(
-                  alignment: FractionalOffset.bottomCenter,
-                  child: Container(
-                    child: BottomBar(),
-                ),
-                ),
-            ),
-       ],
-      )
-    );
-  
-  
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[Center(child: Text('Track Page'))],
+      ),
+    ));
   }
 }
